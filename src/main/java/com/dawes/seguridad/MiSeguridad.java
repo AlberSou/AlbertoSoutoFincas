950044ee10fb0fe90cb7652ea46513f9a6782555
Alberto Souto <albertosmgi3h@gmail.com>
2023-05-31 20:08:14 +0200

    1

---
+++ src/main/java/com/dawes/seguridad/MiSeguridad.java
@@ -0,0 +1,60 @@
+package com.dawes.seguridad;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.dawes.serviciosimpl.ServicioUsuarioImpl;
+
+@Configuration
+@EnableWebSecurity
+public class MiSeguridad {
+	@Autowired
+	ServicioUsuarioImpl su;
+
+
+	@Bean
+	public BCryptPasswordEncoder encripta() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filtroCompleto(HttpSecurity http) throws Exception {
+
+		// autentificamos
+		http.getSharedObject(AuthenticationManagerBuilder.class).userDetailsService(su)
+				.passwordEncoder(encripta());
+
+		// autorizamos a user acceso a la carpeta user
+
+		http.authorizeHttpRequests().requestMatchers("/fincas/fincasmostrar").hasAnyRole("USER", "ADMIN").and().exceptionHandling()
+				.accessDeniedPage("/403");
+
+		;
+
+		// autorizamos a user admin acceso a la carpeta admin
+
+		http.authorizeHttpRequests().requestMatchers("/fincas/**").hasRole("ADMIN").and().exceptionHandling()
+				.accessDeniedPage("/403");
+
+		;
+
+		// el raiz , index, login, 403 no requieren autentificación
+		http.authorizeHttpRequests().requestMatchers("/", "/fincas/fincasmostrar", "/login", "/403","/fincas/plano/**").permitAll().anyRequest()
+				.authenticated();
+
+		// para autenticarse utiliza el formulario de login personalizado
+
+		http.formLogin().loginPage("/login").permitAll();
+
+		// programamos el logout
+		http.logout().logoutUrl("/logout").permitAll();
+
+		return http.build();
+	}
+}
