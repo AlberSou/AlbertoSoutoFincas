19635c959c5799b93fbe06e7a2e007d02a2d1aa4
Alberto Souto <albertosmgi3h@gmail.com>
2023-06-05 21:01:42 +0200

    1

--- src/main/resources/static/js/plano.js
+++ src/main/resources/static/js/plano.js
@@ -1,74 +1,75 @@
-//Recocgemos el parametro de la url
+// Recogemos el parámetro de la URL
 const params = new URLSearchParams(window.location.search);
-const idfinca = params.get("idfinca");
-//Funcion para recoger la url de la id finca para obtener los arboles
-async function recogerDatos() {
-	const response = await fetch(`https://alberto-souto-fincas.herokuapp.com/fincas/plano/${idfinca}`);
-	const data = await response.json();
-	sacarDatos(data);
+const idFinca = params.get("idfinca");
+
+// Función para obtener los datos de los árboles
+async function obtenerDatosArboles() {
+  const response = await fetch(`https://alberto-souto-fincas.herokuapp.com/fincas/plano/${idFinca}`);
+  const datos = await response.json();
+  mostrarDatosArboles(datos);
 }
-//Función para sacar los datos, los arboles
-function sacarDatos(data) {
-	const contenedor = document.querySelector(".contenedor");
-	contenedor.innerHTML = "";
-	contenedor.style.display = "grid";
 
-	let maxColumna = 0;
-	let maxFila = 0;
+// Función para mostrar los datos de los árboles
+function mostrarDatosArboles(datos) {
+  const contenedor = document.querySelector(".contenedor");
+  contenedor.innerHTML = "";
+  contenedor.style.display = "grid";
 
-	// Obtener los valores máximos de fila y columna
-	for (let tam of data) {
-		if (tam.columna > maxColumna) {
-			maxColumna = tam.columna;
-		}
-		if (tam.fila > maxFila) {
-			maxFila = tam.fila;
-		}
-	}
-	// Crear las celdas de la cuadrícula
-	for (let fila = 1; fila <= maxFila; fila++) {
-		for (let columna = 1; columna <= maxColumna; columna++) {
-			const cuadricula = document.createElement("div");
-			cuadricula.classList.add("cuadricula");
-			contenedor.appendChild(cuadricula);
-			cuadricula.style.gridRow = fila.toString();
-			cuadricula.style.gridColumn = columna.toString();
-			cuadricula.addEventListener("click", () => {
-			
-			window.location.href = `../arboles/planoforminsertar?filax=${fila}&columnax=${columna}&idfincax=${idfinca}`;
-		})
-		}
-	}
+  let maxColumna = 0;
+  let maxFila = 0;
 
-	// Colocar los árboles en las celdas correspondientes
-	for (let arbol of data) {
-		const tree = document.createElement("div");
-		tree.classList.add("arbol");
+  // Obtener los valores máximos de fila y columna
+  for (let dato of datos) {
+    if (dato.columna > maxColumna) {
+      maxColumna = dato.columna;
+    }
+    if (dato.fila > maxFila) {
+      maxFila = dato.fila;
+    }
+  }
 
-		switch (arbol.estado.toUpperCase()) {
-			case "MUERTO":
-			tree.style.backgroundColor = "black";
-			break;
-			case "SANO":
-			tree.style.backgroundColor = "lightbrown";
-			break;
-			case "ENFERMO":
-			tree.style.backgroundColor = "darkgreen";
-			break;
-		}
-		tree.style.gridRow = (arbol.fila).toString();
-		tree.style.gridColumn = arbol.columna.toString();
+  // Crear las celdas de la cuadrícula
+  for (let fila = 1; fila <= maxFila; fila++) {
+    for (let columna = 1; columna <= maxColumna; columna++) {
+      const cuadricula = document.createElement("div");
+      cuadricula.classList.add("cuadricula");
+      contenedor.appendChild(cuadricula);
+      cuadricula.style.gridRow = fila.toString();
+      cuadricula.style.gridColumn = columna.toString();
+      cuadricula.addEventListener("click", () => {
+        window.location.href = `../arboles/planoforminsertar?filax=${fila}&columnax=${columna}&idfincax=${idFinca}`;
+      });
+    }
+  }
 
-		tree.dataset.idarbol = arbol.idarbol;
+  // Colocar los árboles en las celdas correspondientes
+  for (let arbol of datos) {
+    const arbolElemento = document.createElement("div");
+    arbolElemento.classList.add("arbol");
 
-		tree.addEventListener("click", () => {
-			const idarbol = tree.dataset.idarbol;
-			window.location.href = `../arboles/formmodificar?idarbol=${idarbol}`;
-		});
+    switch (arbol.estado.toUpperCase()) {
+      case "MUERTO":
+        arbolElemento.style.backgroundColor = "black";
+        break;
+      case "SANO":
+        arbolElemento.style.backgroundColor = "lightbrown";
+        break;
+      case "ENFERMO":
+        arbolElemento.style.backgroundColor = "darkgreen";
+        break;
+    }
+    arbolElemento.style.gridRow = arbol.fila.toString();
+    arbolElemento.style.gridColumn = arbol.columna.toString();
 
-		contenedor.appendChild(tree);
-	}
+    arbolElemento.dataset.idArbol = arbol.idarbol;
+
+    arbolElemento.addEventListener("click", () => {
+      const idArbol = arbolElemento.dataset.idArbol;
+      window.location.href = `../arboles/formmodificar?idarbol=${idArbol}`;
+    });
+
+    contenedor.appendChild(arbolElemento);
+  }
 }
-		
-	
-recogerDatos();
+
+obtenerDatosArboles();
